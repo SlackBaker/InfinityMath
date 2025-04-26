9a8c88ec86bade3c9ebccec0cc0c2f7e818ff332
SlackBaker <ostapanonim228@gmail.com>
2025-04-26 20:26:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import InfinityMath.algebra
-from InfinityMath import algebra
-from InfinityMath import trigonometry
-from InfinityMath import calculus
-from InfinityMath import graphics
-from InfinityMath import quadratics_math
+from . import algebra
+from . import trigonometry
+from . import calculus
+from . import quadratics_math
+from . import graphics
 
-print(InfinityMath.__version__)
-#get version
+pi = 3,1415926535897932384626433832795
+e = 2,7182818284590452353602874713527
 
-print(InfinityMath.algebra.div(2,5))
-print(InfinityMath.algebra.pow(3,5))
-# You can use sqrt, sum, substarction and other functions below
-
+__version__ = "1.0.0"
+def show_version():
+    print(f"InfinityMath version: {__version__}")
